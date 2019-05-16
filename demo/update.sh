a7a537ac15833c605a66e55dcb3116ed82ca3feb
WayneSan <waynesan@zerozone.tw>
2019-05-16 10:42:36 +0800

    // WIP

--- demo/update.sh
+++ demo/update.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
+cd ../pf4j-web/
+mvn clean install -DskipTests
+cd -
+
 #Build
-mvn clean package
+mvn -pl org.pf4j.web.demo:pf4j-web-demo,pf4j-web-demo-api clean install -DskipTests
+mvn -pl pf4j-web-demo-csv-plugin,pf4j-web-demo-json-plugin,pf4j-web-demo-xml-plugin clean package -DskipTests
 
 # create the plugins folder
 rm -rf plugins
 mkdir plugins
 
 # copy artifacts to plugins folder
-cp pf4j-web-demo-csv-plugin/target/*.zip plugins/
-cp pf4j-web-demo-json-plugin/target/*.zip plugins/
-cp pf4j-web-demo-xml-plugin/target/*.zip plugins/
+cp ./pf4j-web-demo-csv-plugin/target/*.zip plugins/
+cp ./pf4j-web-demo-json-plugin/target/*.zip plugins/
+cp ./pf4j-web-demo-xml-plugin/target/*.zip plugins/
 
-cd pf4j-web-demo-app
-mvn clean jetty:run
-cd ..
+mvn -pl pf4j-web-demo-app clean jetty:run
